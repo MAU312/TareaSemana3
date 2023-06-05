49731b2c0094be24f39074595ae6e6df7862f32c
mauhi <mauhi@GAMA>
2023-06-05 17:57:01 -0600

    Tarea Semana3

---
+++ src/test/java/com/TareaSemana3/TareaSemana3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.TareaSemana3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TareaSemana3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
